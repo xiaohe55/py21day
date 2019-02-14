8cea53895b79db3668902e836f5fe0455cc8a635
coyote <1185462886@qq.com>
2019-02-14 08:40:38 +0800

    day0

--- demo1.py
+++ demo1.py
@@ -6,6 +6,7 @@
 @function:urlopen
 '''
 from urllib import request
+from urllib import parse
 
 '''
 urlopen
@@ -29,5 +30,28 @@ urlretrieve:下载网页到本地
 # 下载百度网页
 # request.urlretrieve("http://www.baidu.com", 'baidu.html')
 # 下载图片
-request.urlretrieve("https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3780996643,1336778904&fm=26&gp=0.jpg", 'modi.jpg')
+# request.urlretrieve("https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3780996643,1336778904&fm=26&gp=0.jpg", 'modi.jpg')
 
+"""
+urlencode 编码
+"""
+# params = {"name": "张三", "age": 18, "greet": "hello world"}
+# result = parse.urlencode(params)
+# print(result)
+
+# url = "http://www.baidu.com/s"
+# params = {"wd": "刘德华"}
+# qs = parse.urlencode(params)
+# url = url + "?" + qs
+# resp = request.urlopen(url)
+# print(resp.read())
+
+"""
+parse_qs 解码
+"""
+
+params = {"name": "张三", "age": 18, "greet": "hello world"}
+qs = parse.urlencode(params)
+print(qs)
+result = parse.parse_qs(qs)
+print(result)
