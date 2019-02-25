d52025cbbd7f80ef62b99357b2a4390e4258a84d
coyote <1185462886@qq.com>
2019-02-25 08:44:27 +0800

    0225

--- demo3.py
+++ demo3.py
@@ -6,4 +6,30 @@
 @function:
 '''
 
-from urllib import request
+from urllib import request, parse
+
+url = 'https://www.lagou.com/jobs/list_python?labelWords=&fromSearch=true&suginput='
+
+# 错误代码
+# resp = request.urlopen(url)
+# print(resp.read())
+
+# 已经被禁
+# headers = {
+#     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36'}
+# req = request.Request(url, headers=headers)
+# resp = request.urlopen(req)
+# print(resp.read())
+
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36',
+    'Referer': 'https://www.lagou.com/jobs/list_python?city=%E5%8C%97%E4%BA%AC&cl=false&fromSearch=true&labelWords=&suginput='}
+data = {
+    'first': 'true',
+    'pn': 1,
+    'kd': 'python'
+}
+req = request.Request(url, headers=headers, data=parse.urlencode(data).encode("utf-8"), method="POST")
+resp = request.urlopen(req)
+print(resp.read().decode('utf-8'))
